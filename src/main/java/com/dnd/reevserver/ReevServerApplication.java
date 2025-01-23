731a479508707af0b727936534c2b4f92a004fca
RyuTaeUung <aser77992@gmail.com>
2025-01-23 13:17:40 +0900

    feat : start Project!

---
+++ src/main/java/com/dnd/reevserver/ReevServerApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.reevserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReevServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReevServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dnd/reevserver/ReevServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.reevserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReevServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

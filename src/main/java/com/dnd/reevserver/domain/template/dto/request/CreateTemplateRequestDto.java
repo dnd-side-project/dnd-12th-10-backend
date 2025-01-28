ceaed751e3cedbe905f04733c342be96645eb141
RTUnu12 <aser77992@gmail.com>
2025-01-28 15:06:35 +0900

    refactor : Request Dto Record로 변환

--- src/main/java/com/dnd/reevserver/domain/template/dto/request/CreateTemplateRequestDto.java
+++ src/main/java/com/dnd/reevserver/domain/template/dto/request/CreateTemplateRequestDto.java
@@ -1,14 +1,4 @@
 package com.dnd.reevserver.domain.template.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class CreateTemplateRequestDto {
-    private String userId;
-    private String templateName;
-    private String content;
+public record CreateTemplateRequestDto(String userId, String templateName, String content) {
 }
--- src/main/java/com/dnd/reevserver/domain/template/dto/request/FindTemplateByMemberIdRequestDto.java
+++ src/main/java/com/dnd/reevserver/domain/template/dto/request/FindTemplateByMemberIdRequestDto.java
@@ -1,12 +1,4 @@
 package com.dnd.reevserver.domain.template.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class FindTemplateByMemberIdRequestDto {
-    private String userId;
+public record FindTemplateByMemberIdRequestDto(String userId) {
 }

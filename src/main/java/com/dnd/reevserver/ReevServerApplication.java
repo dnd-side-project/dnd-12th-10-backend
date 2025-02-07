610a736814fafdc39f5a73808b1eb7406ff487fb
RTUnu12 <aser77992@gmail.com>
2025-02-07 14:22:51 +0900

    fix : Mixed Content 오류 수정

--- src/main/java/com/dnd/reevserver/ReevServerApplication.java
+++ src/main/java/com/dnd/reevserver/ReevServerApplication.java
@@ -1,10 +1,17 @@
 package com.dnd.reevserver;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url="/", description = "Default Server URL")
+        }
+)
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableJpaAuditing

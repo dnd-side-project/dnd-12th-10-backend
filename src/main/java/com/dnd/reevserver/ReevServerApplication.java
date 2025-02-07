d1786ea0669436653768bca6c0fa5145da357247
RTUnu12 <aser77992@gmail.com>
2025-02-07 14:10:27 +0900

    fix : CORS 오류 수정

--- src/main/java/com/dnd/reevserver/ReevServerApplication.java
+++ src/main/java/com/dnd/reevserver/ReevServerApplication.java
@@ -1,13 +1,10 @@
 package com.dnd.reevserver;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://reevserver.site", description = "기본 도메인 설정")})
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
--- src/main/java/com/dnd/reevserver/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/dnd/reevserver/global/config/swagger/SwaggerConfig.java
@@ -2,15 +2,23 @@ package com.dnd.reevserver.global.config.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
 @Configuration
 public class SwaggerConfig {
+    @Value("${SPRING_SWAGGER_SERVER_URL:https://reevserver.site}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI WonJeongFoodOpenApi() {
         return new OpenAPI()
+                .servers(Collections.singletonList(new Server().url(serverUrl).description("API Server")))
                 .info(new Info()
                         .title("Reev API")
                         .description("Reev 백엔드 API 명세서"));

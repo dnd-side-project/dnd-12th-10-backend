58d5ac3495ab5ec5d6adcfac70c84da673f6f9ec
RTUnu12 <aser77992@gmail.com>
2025-02-07 14:35:59 +0900

    fix : Mixed Content 오류 수정

--- src/main/java/com/dnd/reevserver/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/dnd/reevserver/global/config/swagger/SwaggerConfig.java
@@ -1,11 +1,18 @@
 package com.dnd.reevserver.global.config.swagger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://reevserver.site", description = "프로덕션 서버"),
+                @Server(url = "http://localhost:8080", description = "로컬 서버")
+        })
 @Configuration
 public class SwaggerConfig {
 
@@ -46,6 +53,22 @@ public class SwaggerConfig {
         return GroupedOpenApi.builder()
                 .group("Memo")
                 .pathsToMatch("/api/v1/memo/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi teamGroup(){
+        return GroupedOpenApi.builder()
+                .group("Team")
+                .pathsToMatch("/api/v1/group/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi retrospectGroup(){
+        return GroupedOpenApi.builder()
+                .group("Retrospect")
+                .pathsToMatch("/api/v1/retrospect/**")
                 .build();
     }
 }

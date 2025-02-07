65d67233a869f4e5ae4b0bc88b57c5d955f6093d
RTUnu12 <aser77992@gmail.com>
2025-02-07 13:56:50 +0900

    chore : Health Check 오류 수정

--- src/main/java/com/dnd/reevserver/global/util/HealthCheckController.java
+++ src/main/java/com/dnd/reevserver/global/util/HealthCheckController.java
@@ -4,9 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/health")
+@RestController
 public class HealthCheckController {
-    @GetMapping
+    @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("Health Check OK");
     }

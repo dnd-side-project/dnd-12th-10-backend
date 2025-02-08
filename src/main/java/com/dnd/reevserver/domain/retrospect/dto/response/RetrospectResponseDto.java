ffb61f5f4c1c951d2643ef87eefaf4f60b4fdfe9
youcastle03 <luckthb325@naver.com>
2025-02-09 02:25:14 +0900

    기타 수정

--- src/main/java/com/dnd/reevserver/domain/retrospect/dto/response/RetrospectResponseDto.java
+++ src/main/java/com/dnd/reevserver/domain/retrospect/dto/response/RetrospectResponseDto.java
@@ -3,6 +3,6 @@ package com.dnd.reevserver.domain.retrospect.dto.response;
 import lombok.Builder;
 
 @Builder
-public record RetrospectResponseDto(Long retrosepctId, String title, String content,
+public record RetrospectResponseDto(Long retrospectId, String title, String content,
                                     String userName, String timeString, int likeCount) {
 }

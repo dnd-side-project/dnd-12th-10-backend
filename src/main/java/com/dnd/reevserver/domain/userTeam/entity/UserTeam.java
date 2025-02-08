31aa3c3a9e785867b0ce31d8a541c8164f5be7f0
youcastle03 <luckthb325@naver.com>
2025-02-09 02:08:26 +0900

    refator: userTeam변수명 통일

--- src/main/java/com/dnd/reevserver/domain/userTeam/entity/UserTeam.java
+++ src/main/java/com/dnd/reevserver/domain/userTeam/entity/UserTeam.java
@@ -24,7 +24,7 @@ public class UserTeam extends BaseEntity {
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "team_id", nullable = false)
+    @JoinColumn(name = "group_id", nullable = false)
     private Team team;
 
     @ColumnDefault("false")

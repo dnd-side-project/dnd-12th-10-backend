36aa5871fad4688f4a2a18ffce69c8a9a6c1e5eb
RTUnu12 <aser77992@gmail.com>
2025-02-15 20:53:37 +0900

    refactor : early return 적용

--- src/main/java/com/dnd/reevserver/domain/like/service/LikeService.java
+++ src/main/java/com/dnd/reevserver/domain/like/service/LikeService.java
@@ -29,12 +29,11 @@ public class LikeService {
             likeRepository.removeLike(userId, retrospectId);
             retrospectService.updateLikeCnt(retrospectId, false);
             return false;
-        } else {
-            likeRepository.addLike(userId, retrospectId);
-            retrospectService.updateLikeCnt(retrospectId, true);
-            alertService.sendMessage(member.getName() + "님이 " + retrospect.getTitle() + "에 좋아요를 눌렀습니다. [" + retrospectId + "]");
-            return true;
         }
+        likeRepository.addLike(userId, retrospectId);
+        retrospectService.updateLikeCnt(retrospectId, true);
+        alertService.sendMessage(member.getName() + "님이 " + retrospect.getTitle() + "에 좋아요를 눌렀습니다. [" + retrospectId + "]");
+        return true;
     }
 
     // 특정 유저가 특정 게시글에 좋아요를 눌렀는지 확인

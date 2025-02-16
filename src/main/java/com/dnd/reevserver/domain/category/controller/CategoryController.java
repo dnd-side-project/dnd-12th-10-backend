fe67ee6e28388748fe9199731e2c58116de9afb5
youcaslte03 <luckthb325@naver.com>
2025-02-17 00:25:45 +0900

    feat:Category swagger간단한 설명추가

--- src/main/java/com/dnd/reevserver/domain/category/controller/CategoryController.java
+++ src/main/java/com/dnd/reevserver/domain/category/controller/CategoryController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/category")
 @RequiredArgsConstructor
-public class CategoryController {
+public class CategoryController implements CategoryControllerDocs {
 
     private final CategoryService categoryService;
 

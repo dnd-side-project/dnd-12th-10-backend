41496ada8461729d76595beca85f5ec30c661ae6
youcaslte03 <luckthb325@naver.com>
2025-02-19 02:32:04 +0900

    Revert "feat:response에 추가"
    This reverts commit d818ccf052ba64ec5133c3cb7c4810a4795d8290.

--- src/main/java/com/dnd/reevserver/domain/team/dto/response/TeamResponseDto.java
+++ src/main/java/com/dnd/reevserver/domain/team/dto/response/TeamResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import java.util.List;
 
 @Builder
-public record TeamResponseDto(Long groupId, String groupName, String description, String introduce,
+public record TeamResponseDto(Long groupId, String groupName, String description,
                               int userCount, String recentActString, List<String> categoryNames) {
 
 }

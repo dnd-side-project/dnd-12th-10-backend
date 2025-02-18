6839a3e564d71dfb0e99fc379bc96cb3ef8e6573
youcaslte03 <luckthb325@naver.com>
2025-02-19 00:48:56 +0900

    feat:임시 매핑용 컨트롤러

---
+++ src/main/java/com/dnd/reevserver/domain/userTeam/controller/UserTeamController.java
@@ -0,0 +1,24 @@
+package com.dnd.reevserver.domain.userTeam.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/userteam")
+@RequiredArgsConstructor
+public class UserTeamController {
+
+    private final UserTeamService userTeamService;
+
+    @Operation(summary = "임시 연관관계 매핑용")
+    @PostMapping("/mapping/{userId}/{teadId}")
+    public ResponseEntity<String> mapping(@PathVariable String userId, @PathVariable Long groupId) {
+        String str = userTeamService.mapping(userId,groupId);
+        return ResponseEntity.ok(str);
+    }
+}
---
+++ src/main/java/com/dnd/reevserver/domain/userTeam/service/UserTeamService.java
@@ -0,0 +1,30 @@
+package com.dnd.reevserver.domain.userTeam.service;
+
+import com.dnd.reevserver.domain.member.entity.Member;
+import com.dnd.reevserver.domain.member.service.MemberService;
+import com.dnd.reevserver.domain.team.entity.Team;
+import com.dnd.reevserver.domain.team.service.TeamService;
+import com.dnd.reevserver.domain.userTeam.entity.UserTeam;
+import com.dnd.reevserver.domain.userTeam.repository.UserTeamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserTeamService {
+    private final UserTeamRepository userTeamRepository;
+    private final MemberService memberService;
+    private final TeamService teamService;
+
+    @Transactional
+    public String mapping(String userId, Long groupId){
+        Member member = memberService.findById(userId);
+        Team team = teamService.findById(groupId);
+        UserTeam join = new UserTeam(member,team);
+        userTeamRepository.save(join);
+        member.addUserTeam(join);
+        team.addUserTeam(join);
+        return "굿";
+    }
+}

88c75f4efe68dc754e01ea5cd333421e2f63c1b7
RyuTaeUung <aser77992@gmail.com>
2025-03-22 16:20:24 +0900

    test : CORS 전체 허용

--- src/main/java/com/dnd/reevserver/global/config/security/SecurityConfig.java
+++ src/main/java/com/dnd/reevserver/global/config/security/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
     @Bean
     protected CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.addAllowedMethod(CorsConfiguration.ALL);
         config.addAllowedHeader("*");
         config.setExposedHeaders(List.of("Set-Cookie", "Authorization"));
